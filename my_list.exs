9527a8865ae3c29d27ae60c363260583887ae025
i-whammy <yas.iwami@gmail.com>
2021-05-10 22:32:36 +0900

    solved exercise #7-5-1,2

--- my_list.exs
+++ my_list.exs
@@ -2,7 +2,7 @@ defmodule MyList do
 
   def mapsum(list, func) do
     Enum.map(list, func)
-    |> _reduce(0, Enum.sum())
+    |> _reduce(0, &(&1 + &2))
   end
 
   defp _reduce([], value, _) do
@@ -13,4 +13,23 @@ defmodule MyList do
     _reduce(tail, func.(head, value), func)
   end 
 
+  def max([]) do
+    0
+  end
+
+  def max([head | tail]) do
+    _max(tail, head)
+  end
+
+  def _max([], max_number) do
+    max_number
+  end
+
+  def _max([head | tail], max_number) when head >= max_number do
+    _max(tail, head)
+  end
+
+  def _max([head | tail], max_number) when head < max_number do
+    _max(tail, max_number)
+  end
 end
