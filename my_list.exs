6aab001de9efe6621199760b114269677d203127
i-whammy <yas.iwami@gmail.com>
2021-05-11 08:25:13 +0900

    caesar

--- my_list.exs
+++ my_list.exs
@@ -33,6 +33,22 @@ defmodule MyList do
     _max(tail, max_number)
   end
 
+  def caesar(list, n) do
+    _caesar(list, [], n)
+  end
+
+  defp _caesar([], value, _) do
+    value
+  end
+
+  defp _caesar([head | tail], value, n) when head + rem(n,26) <= 122 do
+    _caesar(tail, value ++ [head + rem(n, 26)],n)
+  end
+
+  defp _caesar([head | tail], value, n) when head + rem(n,26) > 122 do
+    _caesar(tail, value ++ [head + rem(n, 26) - 26],n)
+  end
+
   def span(from, to) do
     _add_to_list([], from, to)
   end
