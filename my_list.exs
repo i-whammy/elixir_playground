8a3315fc05e725b39a96ff156e7b907a41b7eaaa
i-whammy <yas.iwami@gmail.com>
2021-05-10 22:44:54 +0900

    solved ListsAndRecursion -4

--- my_list.exs
+++ my_list.exs
@@ -32,4 +32,17 @@ defmodule MyList do
   def _max([head | tail], max_number) when head < max_number do
     _max(tail, max_number)
   end
+
+  def span(from, to) do
+    _add_to_list([], from, to)
+  end
+
+  def _add_to_list(list, num, num) do
+    [list, num]
+    |> List.flatten
+  end
+
+  def _add_to_list(list, from, to) do
+    _add_to_list([list, from], from + 1, to)
+  end
 end
