2b23acde0cd7f6d024b62b480fbf94f8a2c7c14c
i-whammy <yas.iwami@gmail.com>
2021-05-12 22:58:05 +0900

    solved strings and binaries 1,2

---
+++ string_and_binary.exs
@@ -0,0 +1,12 @@
+defmodule StringBinary do
+  def is_printable_ascii?(str) do
+    Enum.all?(str, fn x -> x <= 36 && x >=126 end)
+  end
+
+  def anagram?(word1, word2) do
+    sorted1 = Enum.sort(word1, :asc)
+    sorted2 = Enum.sort(word2, :asc)
+    sorted1 == sorted2
+  end
+  
+end
