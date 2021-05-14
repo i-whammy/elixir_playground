9245d5db240ac9e9be94f98e904a5e7c0e47d127
i-whammy <yas.iwami@gmail.com>
2021-05-14 22:37:09 +0900

    fizzbuzz

---
+++ fizzbuzz.exs
@@ -0,0 +1,14 @@
+defmodule FizzBuzz do
+  def fizzbuzz(n) do
+    1..n |> Enum.map(&_fizzbuzz/1)
+  end
+
+  defp _fizzbuzz(n) do    
+    case {rem(n,3), rem(n,5)} do
+        {0,0} -> "FizzBuzz"
+        {0,_} -> "Fizz"
+        {_,0} -> "Buzz"
+        {_,_} -> n
+    end
+  end
+end
